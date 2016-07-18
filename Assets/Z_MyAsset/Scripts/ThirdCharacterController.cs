9e2235dd6ded5670bba850eec530c4b2833ec698
Euphonux <zz1322@nyu.edu>
2016-07-17 20:35:58 -0400

    added standard asset

--- Assets/Z_MyAsset/Scripts/ThirdCharacterController.cs
+++ Assets/Z_MyAsset/Scripts/ThirdCharacterController.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class ThirdCharacterController : MonoBehaviour {
 
+	Animator _anim;
+	int _hashSpeed;
+	
+
 	// Use this for initialization
 	void Start () {
-	
+		_anim = GetComponent<Animator>();
+		_hashSpeed = Animator.StringToHash("Speed");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKey(KeyCode.UpArrow)){
+			_anim.SetFloat("Speed", 0.2f);
+		}
 	}
 }
